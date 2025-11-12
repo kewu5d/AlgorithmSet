f901638c0b97bdf174189e0504712f24ab2783c9
kewu <3252004362@qq.com>
2025-11-12 20:35:17 +0800

    lg1387早期编译通过版

--- set/Training_camp/前缀和&差分/1387.cpp
+++ set/Training_camp/前缀和&差分/1387.cpp
@@ -1,65 +1,61 @@
-//读入矩阵，二维数组存，从1下标开始存。从1，1开始遍历，增大最大变长。
-//正方形区间前缀和==变长^2。取max。其中边长max（n, m） 
-
 #include <iostream>
 #include <cstdio>
-#include <cmath>
-#include <algorithm>
 using namespace std;
-//int x1, y1;//正方形的左上角边，右下角（x1+ans, y1+ans），ans就是最大边长
-//**全局变量不宜作循环变量
-int a[105][105];	//array subscript数组下标a[] []不能有空格
-int sum[105][105];
+//求区间和 --二维
+//正方形面积 等于 区间和 
+int n, m;
+int a[105][105];
+int suma[105][105];
+int flag;	//因为该矩阵有可能全为0 
 int main()
 {
-	int n, m;cin >> n>> m;//n行m列 
-	int ans = 0, flag = 0;
-	for (int i=1; i<= n; i++)
-	for (int j= 1;j <=m; j++)	
+	cin >> n >> m;
+	for (int i = 1; i <= n; i++)
+	for (int j = 1; j <= m; j++)
 	{
 		cin >> a[i][j];
-		if (a[i][j] == 1)flag = 1;//边长至少为1 
+		//排除0阵
+		if (a[i][j] == 1)
+		{
+			flag = 1;
+		}
 	}
-	//特殊情况--0阵 
-	if (flag == 0)
+	//求suma
+	for (int i = 1; i <= n; i++)
 	{
+		for (int j = 1; j <= m; j++)
+		{
+			suma[i][j] = suma[i-1][j] + suma[i][j-1] - suma[i-1][j-1] + a[i][j];
+		}
+	}
+	
+	 
+	int ans, r;	//答案和边长 
+	if (flag == 0){
+		//说明为0阵
 		cout << 0 << endl;
 		return 0;
 	}
-	//前缀和数组
-	for (int i=1; i <= n; i++)
-	for (int j= 1;j <=m; j++)
-	{
-		sum[i][j] = sum[i][j-1] + sum[i-1][j] + a[i][j] - sum[i-1][j-1];
-	}
-	//**倒着枚举**右下角x1+ans, y1+ans 
-	//通过右下角-左上角的前缀和区间均为1，判断ans的合法性
-	ans = min(n, m);
-	while (ans >=1)
-	{
-		int t = ans-1;//x1+t长度为边长ans
-		for (int x1 = 1; x1+t <=n; x1++)
+	ans = 1;//至少是1
+	//枚举区间正方形和的右下角
+	//枚举边长
+	r = 2;//从2开始
+	int x;
+	while (r <= min(n, m))
+	{//最左边的一个正方形，右下角就是[r][r] 
+		for (int i = r; i <= n; i++)
 		{
-			for (int y1 = 1; y1+t <=m; y1++)
+			for (int j = r; j <= m; j++)
 			{
-				int x = sum[x1+t][y1+t] - sum[x1+t][y1-1] - sum[x1-1][y1+t] + sum[x1-1][y1-1];
-				//cout << x << ' '; 
-				if (x == ans*ans)
+				x = suma[i][j] - suma[i][j-r] - suma[i-r][j] + suma[i-r][j-r];
+				if (x == r * r)
 				{
-					cout << ans << endl;
-					return 0;//倒着枚举的好处！
+					ans = max(ans, r);
 				}
 			}
 		}
-		ans--;
+		r++;
 	}
+	cout << ans << endl;
 	return 0;
 }
-/*
-4 4
-0 1 1 1
-1 1 1 0
-0 1 1 0
-1 1 0 1
-
-*/
