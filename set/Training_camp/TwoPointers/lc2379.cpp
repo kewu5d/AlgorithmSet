4b81c31a7a42c28f917d1fc92210119e0e7c465c
kewu <3252004362@qq.com>
2025-11-19 15:18:24 +0800

    优化leetcode2379，并尝鲜了gemini3pro

--- set/Training_camp/TwoPointers/lc2379.cpp
+++ set/Training_camp/TwoPointers/lc2379.cpp
@@ -3,34 +3,29 @@
 #include <string>
 using namespace std;
 //char blocks[105]; 		//= {'\0'}
-int minimumRecolors(string blocks, int k) {
-    int l= 0, r= k-1;	//string从0开始存储 
-	int ans = k; // 初始化为可能的最大值（全白的情况）//******************************
-	int x=0;
-	//cout << blocks.length() << endl;
-	while (r <= blocks.length())
+int minimumRecolors(string blocks, int k)
+{
+	int l=0, r = l+k-1;//固定窗口长度为k
+	int ans = k+1, cnt_w= 0;//[l, r]内W的个数 
+	for (int i=l; i<=r; i++)
 	{
-		for (int i = l; i <= r; i++)
-		{		
-			if (blocks[i] == 'W')
-			{
-				x++;
-			}
-		}
-		ans = min(ans, x);
-		x=0;
-		l++;
-		r++;
+		if (blocks[i] == 'W')cnt_w++;
 	}
-	return ans;
+	ans = min(cnt_w, ans);
+	while (r < blocks.length()-1)
+	{
+		if (blocks[l++] == 'W')cnt_w--;//移动左指针
+		if (blocks[++r] == 'W')cnt_w++;
+		ans = min(ans, cnt_w);//找窗口内最少W数
+	}
+	return ans; 
 }
 int main()
 {
 	string blocks;
 	int k;
 	scanf("%d", &k);
-	getchar();	//***************************************************都行 
-	//cin.ignore();
+	getchar();	//cin.ignore();//*******************************都行 
 	getline(cin, blocks);
 	//cout << blocks  << endl;
 	int ans =  minimumRecolors(blocks, k);
