fc30a57a19b48dfb794285c93d831e63d23a1dbd
kewu <3252004362@qq.com>
2025-11-25 17:07:30 +0800

    重审lg4995

--- set/Training_camp/greedy/lg4995.cpp
+++ set/Training_camp/greedy/lg4995.cpp
@@ -2,7 +2,7 @@
 #include <cstdio>
 #include <algorithm>
 using namespace std;
-long long ans;// 
+long long ans;//平方就是8次方再相加，记得开long long
 int h[305];//h[0]也就是地面高度为0 
 int main()
 {
@@ -15,9 +15,10 @@ int main()
 	//排序
 	sort(h, h+n+1);//因为左闭右开，所以这里+1
 	int t=n, b=0;
+	//模拟左右横跳
 	while (b < t) 
 	{
-		ans += (h[t]-h[b]) * (h[t]-h[b]);
+		ans += (h[t]-h[b]) * (h[t]-h[b]);//注意防平方的溢出
 		b++;
 		ans += (h[t]-h[b]) * (h[t]-h[b]);
 		t--;
