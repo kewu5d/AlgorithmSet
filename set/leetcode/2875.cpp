6fbe8d9a69db98915b80c010cf068e8c8b3d1a87
kewu <3252004362@qq.com>
2025-12-23 21:36:19 +0800

    笔记：lc2875题解

---
+++ set/leetcode/2875.cpp
@@ -0,0 +1,45 @@
+/*
+ * @lc app=leetcode.cn id=2875 lang=cpp
+ *
+ * [2875] 无限数组的最短子数组
+ */
+#include <bits/stdc++.h>
+#include <vector>
+#include <unordered_map>
+#include <algorithm>
+using namespace std;
+// @lc code=start
+class Solution {
+public:
+    int minSizeSubarray(vector<int>& nums, int target) {
+        long long total = accumulate(nums.begin(), nums.end(), 0);
+        int k = target / total;
+        int rem = target % total;//remainder余数
+        int n = nums.size();
+        int ans = target + 1, l, r;//sum = 0;
+        long long sum = 0;//sum至多小于两个total，需要用long long
+        for (l=r=0; r < 2*n; r++)
+        {
+            sum += nums[r % n];//for判断条件+取余实现遍历两次nums数组
+            while (sum > rem)
+            {
+                sum -= nums[l % n];
+                l++;
+            }
+            if (sum == rem)
+            {
+                ans = min(ans, r-l+1);
+            }
+        }
+        return ans > target? -1: ans + k * n;
+    }
+};
+// @lc code=end
+int main()
+{
+    Solution Sol;
+    vector<int> a = {2,1,5,7,7,1,6,3};
+    int ans = Sol.minSizeSubarray(a, 39);
+    cout << ans << endl;
+    return 0;
+}
