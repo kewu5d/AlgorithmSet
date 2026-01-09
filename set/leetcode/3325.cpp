edaec75e4bec57fb76d69e9dc3ebdcc5620def1f
kewu <3252004362@qq.com>
2026-01-09 21:08:10 +0800

    大胆优化c3325

--- set/leetcode/3325.cpp
+++ set/leetcode/3325.cpp
@@ -6,27 +6,28 @@ using namespace std;
 class Solution {
 public:
     int numberOfSubstrings(string s, int k) {
-        int ch[130]{};
-        int cnt = 0;//至少出现k次的字符数
-        int l, r, ans = 0;
-        for (l=r=0; r < s.length(); r++)
+        int ch[26]{};
+        //int cnt = 0;//至少出现k次的字符数
+        int ans = 0, l = 0;
+        for (char c: s)
         {
-            ch[s[r]]++;
-            if (ch[s[r]] == k)cnt++;
-            while (cnt >= 1)
+            ch[c - 'a']++;
+            //if (ch[s[r]] == k)cnt++;
+            while (ch[c - 'a'] == k)
             {
-                if (ch[s[l]] == k)
-                {
-                    ch[s[l]]--;
-                    cnt--;
-                }
-                else ch[s[l]]--;
+                ch[s[l] - 'a']--;
                 l++;
             }
             ans += l;
         }
         return ans;
     }
+    /*
+    大胆点：
+    1、26个字母，只开26个单位空间来存，用char - 'a'作下标
+    2、不单独弄一个变量记录符合条件的字符数，刚刚添加完ch[c - 'a']++，立马判断是否合法
+    3、直接用==，不要>=。因为出while循环，此时窗口一定不合法，不会出现 > 的情况
+    */
 };
 
 int main()
